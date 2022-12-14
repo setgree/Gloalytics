06b75904288a097a255b9e3e2a1d7df13852e91c
Seth Green <setgree@gmail.com>
2022-12-14 09:59:57 -0500

    much better structure!

--- scripts/visualizing Glo impact.R
+++ scripts/visualizing Glo impact.R
@@ -1,34 +1,28 @@
-#' ### visualizing Glo impact
-rm(list = ls())
-library(ggplot2) 
-library(dplyr, warn.conflicts = FALSE)
+options(scipen = 99)
+library(ggplot2)
+library(ggrepel)
+library(dplyr)
 
-glo_crossover_point <- 22240000 # see Glo-impact-at-different-scales
-largest_stablecoin <- 68000000000
-GD_doubling_point <- 8307566800
- 
-glo_stages <- tribble(~"inflection_point", ~"amount",
-                       "Glo crossover point", glo_crossover_point,
-                      "GiveDirectly doubling point", GD_doubling_point,
-                      "largest stablecoin", largest_stablecoin) %>% 
-  mutate(money_donated = amount * .03) 
+df <- tribble(~"milestone", ~"market_cap", ~"people_helped",
+              "starting point", 0, 0,
+              "sid's investment pays off", 22224000, 1307)
 
-
-# the plot, so far
-glo_stages %>%
-           ggplot(aes(x = inflection_point, 
-                      y = amount)) +
-                    geom_col( fill = "cyan") +
-  ylab("market cap") +
-  xlab("Glo at different scales") +
-  scale_y_continuous(sec.axis = 
-                       sec_axis(~./17000, name = "people helped")) +
+p <- ggplot(data = df,
+            aes(x = market_cap,
+                y = people_helped,
+                label = milestone)) +
+  geom_point(color = "cyan") +
+  geom_abline(intercept = 0, slope = 1/17000, 
+              color = "light blue", lty = "dashed") +
+  xlab("market cap") +
+  ylab("people helped") +
+  geom_label_repel(max.iter = 10000, 
+                   colour = "black", family = 'Helvetica', # fontface = "bold", 
+                   box.padding = unit(0.25, "lines"), 
+                   point.padding = unit(.25, "lines"), 
+                   alpha = .75,
+                   segment.colour = 'gray',
+                   size = 4,
+                   force = 4,
+                   show.legend = FALSE) +
   theme_minimal()
-
-# losing faith in this infographic idea. The technical problem I’m having
-# trouble solving is having 3% of each bar highlighted in a different color, I’m
-# sure someone who really knows what they’re doing with ggplot2 would solve this
-# in a moment but I’m not that person. The conceptual problem is that the scales
-# are so different from each other that it’s effectively impossible to compare
-# them on the same scale as each other without putting a variable into log() and
-# then it becomes much, much harder to understand at a glance. 
